89e8527ab379e455d65c09052cca20dc5771b1f3
JMG <jmg173@naver.com>
2024-04-24 23:09:04 +0900

    리프레시 토큰 인터셉터 응답  추가

--- src/app/common/commonAxios.tsx
+++ src/app/common/commonAxios.tsx
@@ -2,12 +2,6 @@
 import axios, { AxiosInstance, AxiosRequestConfig  } from 'axios';
 import {USE_BACK_URL} from '../../../constants';
 
-const token = () => {
-  const localStorageToken = localStorage.getItem('a');
-  console.log(localStorageToken);
-  return localStorageToken;
-}
-
 export const customAxios: AxiosInstance = axios.create({
   baseURL: `${USE_BACK_URL}`, // 기본 서버 주소 입력
   headers: {
@@ -19,7 +13,7 @@ export const customAxios: AxiosInstance = axios.create({
 customAxios.interceptors.request.use(
   (config) => {
     // 현재 토큰 값을 localStorage에서 가져옵니다.
-    const currentToken = token();
+    const currentToken = localStorage.getItem('a');
     if (currentToken) {
       // Authorization 헤더에 현재 토큰 값을 설정합니다.
       config.headers['Authorization'] = `Bearer ${currentToken}`;
@@ -27,6 +21,19 @@ customAxios.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+customAxios.interceptors.response.use(
+  (response) => {
+    // 응답 200번대 status일 때 응답 성공 직전 호출
+    // 3. 이 작업 이후 .then()으로 이어진다
+    return response;
+  },
+  (error) => {
+    // 응답 200번대가 아닌 status일 때 응답 에러 직전 호출
+    // 4. 이 작업 이후 .catch()로 이어진다
     return Promise.reject(error);
   }
 );
@@ -51,14 +58,11 @@ export const commonAxios = async (method: "get" | "post" | "put" | "patch" | "de
             'Content-Type': 'application/json'
           }
         })
-        console.log(result.data.result.accessToken);
 
         localStorage.setItem("a", result.data.result.accessToken);
-        console.log(localStorage.getItem('a'));
         
         const reResponse = await customAxios[method](url, body);
 
-        console.log(reResponse);
         return reResponse; // 성공적인 응답을 반환합니다.
       } catch (error:any) {
         window.location.href = "/login";
@@ -75,29 +79,6 @@ export const commonAxios = async (method: "get" | "post" | "put" | "patch" | "de
 };
 
 
-
-export const refreshToken = (error:any):any => {
-
-  const accessToken:any = localStorage.getItem('a');
-  // console.log(accessToken)
-  if(error?.response.status === 401){
-    // console.log(accessToken)
-    localStorage.setItem('a', accessToken);
-    axios.get(USE_BACK_URL + '/post/api/borads?page=1&size=10',{
-      headers: {
-        'Authorization': 'Bearer ' + accessToken,
-        'Content-Type': 'application/json'
-      }
-    })
-      .then( response => {
-        console.log(response)
-          
-      })
-  }
-  
-  
-  return null;
-}
 
 
 
